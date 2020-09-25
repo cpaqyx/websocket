0aa90294737a546c8a25bf9b7f310f9ac3a6d022
cpaqyx <cpaqyx@aliyun.com>
2020-09-25 09:28:55 +0800

    更新负载均衡课件

---
+++ 5.4spring cloud gateway微服务负载均衡（编码）/code/fastwave-cloud-stomp/src/main/java/fastwave/cloud/stomp/WebSocketServerApplication.java
@@ -0,0 +1,11 @@
+package fastwave.cloud.stomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketServerApplication.class, args);
+	}
+}
