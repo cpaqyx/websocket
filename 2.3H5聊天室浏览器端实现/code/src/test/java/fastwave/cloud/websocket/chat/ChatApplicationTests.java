6453164343dbac32c482a24cdc6cf239b909be14
cpaqyx <cpaqyx@aliyun.com>
2020-11-08 12:47:47 +0800

    更新第二章所有课件

---
+++ 2.3H5聊天室浏览器端实现/code/src/test/java/fastwave/cloud/websocket/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package fastwave.cloud.websocket.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
